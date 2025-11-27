02ae237d64e7e6b377158292a3aede0a34c4e366
gq97a6 <sz.kg@protonmail.com>
2025-11-27 05:45:40 +0100

    Removed mappers update after database has been populated.

--- src/test/kotlin/net/labcluster/crm/server/SchemaTest.kt
+++ src/test/kotlin/net/labcluster/crm/server/SchemaTest.kt
@@ -2,43 +2,21 @@ package net.labcluster.crm.server
 
 import io.quarkus.test.junit.QuarkusTest
 import jakarta.transaction.Transactional
-import org.labcluster.crm.server.EntityMapper.toEntity
+import org.junit.jupiter.api.Test
 import org.labcluster.crm.server.Repository
-import org.labcluster.crm.shared.Mock
+import org.labcluster.crm.server.entity.*
 
 @QuarkusTest
 class SchemaTest {
 
-    //@Test
-    @Transactional
-    fun deleteAll() {
-        Repository.lesson.deleteAll()
-        Repository.student.deleteAll()
-        Repository.group.deleteAll()
-        Repository.topic.deleteAll()
-        Repository.teacher.deleteAll()
-        Repository.course.deleteAll()
-    }
-
-    //@Test
+    @Test
     @Transactional
     fun populateDatabase() {
-        Mock.students.forEach { Repository.student.persist(it.toEntity()) }
-        Repository.student.flush()
-
-        Mock.teachers.forEach { Repository.teacher.persist(it.toEntity()) }
-        Repository.teacher.flush()
-
-        Mock.topics.forEach { Repository.topic.persist(it.toEntity()) }
-        Repository.topic.flush()
-
-        Mock.courses.forEach { Repository.course.persist(it.toEntity()) }
-        Repository.course.flush()
-
-        Mock.lessons.forEach { Repository.lesson.persist(it.toEntity()) }
-        Repository.lesson.flush()
-
-        Mock.groups.forEach { Repository.group.persist(it.toEntity()) }
-        Repository.group.flush()
+        Repository.student.persist(StudentEntity())
+        Repository.teacher.persist(TeacherEntity())
+        Repository.topic.persist(TopicEntity())
+        Repository.course.persist(CourseEntity())
+        Repository.lesson.persist(LessonEntity())
+        Repository.group.persist(GroupEntity())
     }
 }
